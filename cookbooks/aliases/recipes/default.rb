9b23df4b1ea56e1eecb1a9735dca1b928637baa9
elem <elem@elem.(none)>
2013-12-16 20:25:48 -0800

    Added alias cookbook

---
+++ cookbooks/aliases/recipes/default.rb
@@ -0,0 +1,14 @@
+# Alias `h` to go home
+magic_shell_alias 'h' do
+    command 'cd ~'
+end
+
+# Alias `sites` to cd into apache
+magic_shell_alias 'sites' do
+    command "cd #{node['apache']['dir']}/sites-enabled"
+end
+
+# Set Nano as the default editor
+magic_shell_environment 'EDITOR' do
+    value 'nano'
+end
